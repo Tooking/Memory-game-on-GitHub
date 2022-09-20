a992b6713b68939328d1215276e5b4e501d0c748
unknown <andreytseshko@gmail.com>
2022-09-20 20:08:04 +0300

    Memory game

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const cards = document.querySelectorAll('.content-game-card');
+const cards = document.querySelectorAll('.content-game-card-open');
 const letters = ['a','a','b','b','c','c','d','d','e','e','f','f','g','g','h','h','i','i','j','j','k','k','l','l','m','m','n','n','o','o','p','p','q','q','r','r','s','s','t','t','u','u','v','v','w','w','x','x','y','y','z','z'];
-
+const container = document.querySelector('.content-game-container');
 
 function createTheCards(count){
 
@@ -21,7 +21,23 @@ for(countCycles = 0; countCycles <= 35; countCycles++){
     cards[numberOfCard].innerText = value;
 }
 
+container.addEventListener('click', (e) =>{
+  if (!e.target.classList.contains('content-game-container')){
+    theTarget = e.target.classList.remove('content-game-card-close');
+    console.log(theTarget);
+  }
+});
 
-  cards.addEventListener('click', ()=>{
-    cards.classList.toggle('.content-game-card:before');
-  })
+function blockAnotherCards(e){
+  e.currentTarget.setAttribute()
+}
+
+function checkTheCards(firstCard, secondCard){
+  if (firstCard.value === secondCard.value){
+    firstCard.setAttribute('disabled', 'disabled');
+    secondCard.setAttribute('disabled', 'disabled');
+  }else{
+    firstCard.classList.add('content-game-card-close');
+    secondCard.classList.add('content-game-card-close');
+  }
+}
